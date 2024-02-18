65596e0efc61e597ea41fa9c7fbde6be4d34b287
Samuel Jeffman <sjeffman83@outlook.com>
2024-02-18 16:59:38 +0100

    Added class BMICalculator

---
+++ Assignment3/BMICalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class BMICalculator
+    {
+        private string? name = "No name";
+        private double height = 0;
+        private double weight = 0;
+        private UnitTypes unit;
+
+
+    }
+}
