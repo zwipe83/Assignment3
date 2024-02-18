e5d3b16924234d9f906dc1c29a036262caae834a
Samuel Jeffman <sjeffman83@outlook.com>
2024-02-18 18:15:41 +0100

    Added Properties and regions

--- Assignment3/BMICalculator.cs
+++ Assignment3/BMICalculator.cs
@@ -8,11 +8,22 @@ namespace Assignment3
 {
     internal class BMICalculator
     {
+        #region Fields
         private string? name = "No name";
         private double height = 0;
         private double weight = 0;
         private UnitTypes unit;
+        #endregion
 
+        #region Properties
+        public string? Name { get { return name; } set { name = value; } }
+        public double Height { get { return height; } set { height = value; } }
+        public double Weight { get { return weight; } set { weight = value; } }
+        public UnitTypes Unit { get { return unit; } set { unit = value; } }
+        #endregion
+
+        #region Methods
+        #endregion
 
     }
 }
