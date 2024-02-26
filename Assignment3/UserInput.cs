59c91bbd4af6034e5bcf6318ce01ca2a944c6537
Samuel Jeffman <sjeffman83@outlook.com>
2024-02-26 20:03:33 +0100

    Greatly simplified methods Removed exception handling

--- Assignment3/UserInput.cs
+++ Assignment3/UserInput.cs
@@ -1,111 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Assignment3
+﻿namespace Assignment3
 {
     internal class UserInput
     {
+        /// <summary>
+        /// Read a text input and return it as a string
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
         public string ReadTextAsString(string text)
         {
-            try
+            if (string.IsNullOrWhiteSpace(text))
             {
-                if (!string.IsNullOrEmpty(text))
-                {
-                    text = text.Trim();
-                }
-                else
-                {
-                    if (text == null)
-                    {
-                        throw new ArgumentNullException(nameof(text), "Name cannot be Null");
-                    }
-                    if (text == string.Empty)
-                    {
-                        throw new ArgumentException("Name can not be empty.", nameof(text));
-                    }
-                }
-                return text;
+                MessageBox.Show("Input cannot be null or empty.");
+                return null; // TODO: Should this really return null?
             }
-            catch (ArgumentNullException argumentNullException)
-            {
-                MessageBox.Show(argumentNullException.Message);
-                return null;
-            }
-            catch (ArgumentException argumentException)
-            {
-                MessageBox.Show(argumentException.Message);
-                return null;
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show(exception.Message);
-                return null;
-            }
+
+            return text.Trim();
         }
+
+        /// <summary>
+        /// Read a text input and return it as a double
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
         public double ReadTextAsDouble(string text)
         {
-            try
+            if (string.IsNullOrWhiteSpace(text))
             {
-                if (!string.IsNullOrEmpty(text))
-                {
-                    text = text.Trim();
-                }
-                else
-                {
-                    if (text == null)
-                    {
-                        throw new ArgumentNullException(nameof(text), "Name cannot be Null");
-                    }
-                    if (text == string.Empty)
-                    {
-                        throw new ArgumentException("Value can not be empty.", nameof(text));
-                    }
-                }
+                MessageBox.Show("Input cannot be null or empty.");
+                return 0.0;
+            }
 
-                if(Double.TryParse(text, out double dbl))
-                {
-                    return dbl;
-                }
-                else
-                {
-                    throw new Exception("Double.TryParse() failed.");
-                }
-            }
-            catch (ArgumentNullException argumentNullException)
-            {
-                MessageBox.Show(argumentNullException.Message);
-                return 0.0;
-            }
-            catch (ArgumentException argumentException)
-            {
-                MessageBox.Show(argumentException.Message);
-                return 0.0;
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show(exception.Message);
-                return 0.0;
-            }
-        }
-
-        public void SaveName(BMICalculator bmi, string name)
-        {
-            bmi.Name = name;
-        }
-        public void SaveHeight(BMICalculator bmi, double height)
-        {
-            bmi.Height = height;
-        }
-        public void SaveWeight(BMICalculator bmi, double weight)
-        {
-            bmi.Weight = weight;
-        }
-        public void SaveUnit(BMICalculator bmi, UnitTypes unit)
-        {
-            bmi.Unit = unit;
+            return double.TryParse(text.Trim(), out double dbl) ? dbl : 0.0;
         }
     }
 }
