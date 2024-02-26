f5a294835d84df617dca64e3126dcdb610fce65a
Samuel Jeffman <sjeffman83@outlook.com>
2024-02-26 20:02:50 +0100

    Added more units

--- Assignment3/UnitTypes.cs
+++ Assignment3/UnitTypes.cs
@@ -1,16 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace Assignment3
 {
     #region Enums
+    /// <summary>
+    /// Unit types
+    /// </summary>
     internal enum UnitTypes
     {
-        Metric = 0,
+        Metric,
         Imperial
+    }
+    #endregion
+    #region Classes
+    internal static class Metric
+    {
+        /// <summary>
+        /// Metric length units
+        /// </summary>
+        internal enum Length
+        {
+            [Description("m")]
+            Meters,
+            [Description("cm")]
+            Centimeters,
+            [Description("mm")]
+            Millimeters
+        }
+
+        /// <summary>
+        /// Metric weight units
+        /// </summary>
+        internal enum Weight
+        {
+            [Description("kg")]
+            Kilograms,
+            [Description("g")]
+            Grams
+        }
+    }
+
+    internal static class Imperial
+    {
+        /// <summary>
+        /// US length units
+        /// </summary>
+        internal enum Length
+        {
+            [Description("ft")]
+            Feet,
+            [Description("in")]
+            Inches
+        }
+
+        /// <summary>
+        /// US weight units
+        /// </summary>
+        internal enum Weight
+        {
+            [Description("lbs")]
+            Pounds
+        }
     }
     #endregion
 }
